278e2fb6558ff299adc1294cf3aa6b63ca1ac72d
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-12 00:58:19 +0530

    restructured my backend with express router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const Todo = require("./models/todo");
+const todosRoutes = require("./routes/todos");
 
 mongoose.connect("mongodb+srv://shreyansh:ReCsgb9iRYria9fr@cluster0.1jotb.mongodb.net/todo-app?retryWrites=true&w=majority").then(() => {
     console.log("Database Connected!");
@@ -27,72 +27,6 @@ app.use((req, res, next) => {
     next(); 
 });
 
-app.post("/api/todos", (req, res, next) => {
-    // console.log(req.body);
-    const todo = new Todo({
-        title: req.body.title,
-        content: req.body.content,
-        dateCreated: req.body.dateCreated
-    });
-    todo.save().then((result) => {
-        res.status(201).json({
-            message: "Todo successfully created",
-            todoId: result._id
-        });
-    });
-    // console.log(todo);
-});
-
-app.get("/api/todos",(req, res, next) => {
-    Todo.find((err, docs) => {
-        // console.log(docs);
-        // Response method is in this block becoz fetching documents is an async task.
-        res.status(200).json({
-            message: "Todos fetched successfully",
-            todos: docs
-        });
-    });
-});
-
-app.delete("/api/todo/:id", (req, res, next) => {
-    console.log(req.params.id);
-    Todo.deleteOne({_id: req.params.id}).then((result) => {
-        console.log(result);
-        res.status(200).json({
-            message: "Todo deleted successfully"
-        });
-    });
-});
-
-app.get("/api/todo/:id", (req, res, next) => {
-    // console.log(req.params.id);
-    Todo.findById(req.params.id).then((result) => {
-        // console.log(result);
-        res.status(200).json({
-            todo: result
-        });
-    }).catch((reason) => {
-        // console.log(reason);
-        res.status(404).json({
-            message: "Todo with this id not found"
-        });
-    });
-});
-
-app.patch("/api/todo/:id", (req, res, next) => {
-    console.log(req.body);
-    console.log(req.params.id);
-    const todo = new Todo({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Todo.updateOne({_id: req.params.id}, todo).then((result) => {
-        // console.log(result);
-        res.status(200).json({
-            message: "Todo updated successfully"
-        });
-    });
-});
+app.use(todosRoutes);
 
 module.exports = app;
---
+++ routes/todos.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/todo");
+
+router.post("/api/todos", (req, res, next) => {
+    // console.log(req.body);
+    const todo = new Todo({
+        title: req.body.title,
+        content: req.body.content,
+        dateCreated: req.body.dateCreated
+    });
+    todo.save().then((result) => {
+        res.status(201).json({
+            message: "Todo successfully created",
+            todoId: result._id
+        });
+    });
+    // console.log(todo);
+});
+
+router.get("/api/todos",(req, res, next) => {
+    Todo.find((err, docs) => {
+        // console.log(docs);
+        // Response method is in this block becoz fetching documents is an async task.
+        res.status(200).json({
+            message: "Todos fetched successfully",
+            todos: docs
+        });
+    });
+});
+
+router.delete("/api/todo/:id", (req, res, next) => {
+    console.log(req.params.id);
+    Todo.deleteOne({_id: req.params.id}).then((result) => {
+        console.log(result);
+        res.status(200).json({
+            message: "Todo deleted successfully"
+        });
+    });
+});
+
+router.get("/api/todo/:id", (req, res, next) => {
+    // console.log(req.params.id);
+    Todo.findById(req.params.id).then((result) => {
+        // console.log(result);
+        res.status(200).json({
+            todo: result
+        });
+    }).catch((reason) => {
+        // console.log(reason);
+        res.status(404).json({
+            message: "Todo with this id not found"
+        });
+    });
+});
+
+router.patch("/api/todo/:id", (req, res, next) => {
+    console.log(req.body);
+    console.log(req.params.id);
+    const todo = new Todo({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    Todo.updateOne({_id: req.params.id}, todo).then((result) => {
+        // console.log(result);
+        res.status(200).json({
+            message: "Todo updated successfully"
+        });
+    });
+});
+
+module.exports = router;
