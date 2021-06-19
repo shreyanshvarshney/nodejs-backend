7477807bef4c351a680e7d049a3321eaf5c3bccc
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-19 15:09:57 +0530

    added User Controller for handling routes logic

---
+++ controllers/user.js
@@ -0,0 +1,83 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+// Another way to export in Nodejs in which -> exports.myCustomName = (this function I want to export)
+// Here exports will work as an Object which has two properties we mentions createUser and loginUser of type functions
+exports.createUser = (req, res, next) => {
+    if (!req.body.name || !req.body.email || !req.body.password) {
+        return res.status(400).json({
+            error: "Missing required fields"
+        });
+    }
+    bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+        .then((result) => {
+            res.status(201).json({
+                message: "User created successfully",
+                data: result
+            });
+        })
+        .catch((reason) => {
+            res.status(500).json({
+                message: "Invalid Authentication Credentials!"
+            });
+        });
+    })
+    .catch((reason) => {
+        res.status(500).json({
+            error: reason
+        });
+    });
+}
+
+exports.userLogin = (req, res, next) => {
+    let user; // This will be a document of a verified user matched email.
+    User.findOne({email: req.body.email})
+    .then((result) => {
+        // result will be the user document of email equal to res.body.email
+        // console.log(result);
+        if (!result) {
+            return res.status(401).json({
+                message: "Authentication Failed."
+            });
+        }
+        user = result;
+        return bcrypt.compare(req.body.password, result.password);
+        // Chaining of promises
+    })
+    .then((result) => {
+        // result will be a boolean wheather the encrypted pass matches the password send by the client while login.
+        // console.log(result);
+        if (!result) {
+            return res.status(401).json({
+                message: "Authentication Failed."
+            });
+        }
+        // Configuring JWT Token as now user is authenticated/(in our db) for sure
+        const token = jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY, {expiresIn: "1h"});
+        res.status(200).json({
+            token: token,
+            // Time in secs
+            expiresIn: 3600,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    })
+    .catch((err) => {
+        // console.log(err);
+        return res.status(401).json({
+            message: "Invalid Login Credentials!"
+        });
+    });
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
 const router = express.Router();
+
+const UserController = require("../controllers/user");
 
 // To check wheather a particular route is requested by an authenticated user, 
 // I will make my custom middleware to check authentication. Like done for uploading files with multer.
@@ -11,81 +9,11 @@ const router = express.Router();
 // Middleware is just a function which will be executed on an incoming request.
 // My custom middleware func will executed before the final middleware fucn.
 
-router.post("/signup", (req, res, next) => {
-    if (!req.body.name || !req.body.email || !req.body.password) {
-        return res.status(400).json({
-            error: "Missing required fields"
-        });
-    }
-    bcrypt.hash(req.body.password, 10)
-    .then((hash) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then((result) => {
-            res.status(201).json({
-                message: "User created successfully",
-                data: result
-            });
-        })
-        .catch((reason) => {
-            res.status(500).json({
-                message: "Invalid Authentication Credentials!"
-            });
-        });
-    })
-    .catch((reason) => {
-        res.status(500).json({
-            error: reason
-        });
-    });
-});
+// Not executing this function by adding parenthesis UserController.createUser()
+// just passing a refrence of this function UserController.createUser
+// And express will therefore register this function and execute it whenever a request reaches to this route.
+router.post("/signup", UserController.createUser);
 
-router.post("/login", (req, res, next) => {
-    let user; // This will be a document of a verified user matched email.
-    User.findOne({email: req.body.email})
-    .then((result) => {
-        // result will be the user document of email equal to res.body.email
-        // console.log(result);
-        if (!result) {
-            return res.status(401).json({
-                message: "Authentication Failed."
-            });
-        }
-        user = result;
-        return bcrypt.compare(req.body.password, result.password);
-        // Chaining of promises
-    })
-    .then((result) => {
-        // result will be a boolean wheather the encrypted pass matches the password send by the client while login.
-        // console.log(result);
-        if (!result) {
-            return res.status(401).json({
-                message: "Authentication Failed."
-            });
-        }
-        // Configuring JWT Token as now user is authenticated/(in our db) for sure
-        const token = jwt.sign({email: user.email, id: user._id}, process.env.JWT_KEY, {expiresIn: "1h"});
-        res.status(200).json({
-            token: token,
-            // Time in secs
-            expiresIn: 3600,
-            user: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        });
-    })
-    .catch((err) => {
-        // console.log(err);
-        return res.status(401).json({
-            message: "Invalid Login Credentials!"
-        });
-    });
-});
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
