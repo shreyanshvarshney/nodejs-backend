14974dd5d657c5e3b5abfa5dd6ad3eabcb23c5f5
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-21 00:39:54 +0530

    some changes in User Model Schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    name: {type: String, required: true, minLength: 3, trim: true},
+    email: {type: String, required: true, unique: true, trim: true},
     password: {type: String, required: true}
 });
 
